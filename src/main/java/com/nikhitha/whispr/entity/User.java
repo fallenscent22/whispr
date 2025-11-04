7302c03f75dd83e0df5cf019afa5937f894812df
fallenscent22 <22641a0522@vaagdevi.edu.in>
2025-11-04 17:52:14 +0530

    spring-boot and initial setup

---
+++ src/main/java/com/nikhitha/whispr/entity/User.java
@@ -0,0 +1,23 @@
+package com.nikhitha.whispr.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/test/java/com/nikhitha/whispr/WhisprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhitha.whispr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
