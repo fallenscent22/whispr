95e4b0e65a9324cf54bd2aba0d1b84c1b17d44ca
fallenscent22 <22641a0522@vaagdevi.edu.in>
2025-11-05 14:13:02 +0530

    frontend integration

--- src/main/java/com/nikhitha/whispr/security/SecurityConfig.java
+++ src/main/java/com/nikhitha/whispr/security/SecurityConfig.java
@@ -74,9 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://whispr.vercel.app",
-    "https://whispr.netlify.app",
-    "https://whispr.onrender.com")); 
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:8080")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
